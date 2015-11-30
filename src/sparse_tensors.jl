b0df9a0a82aa14fc6617fb14615043bfa8c48ee1
Nikolas Tezak <ntezak@stanford.edu>
2015-11-30 10:00:36 -0800

    cleanup, part 1

--- src/sparse_tensors.jl
+++ src/sparse_tensors.jl
@@ -3,9 +3,9 @@
 export TensorOp, DiagonalOp, 
     IdentityOp, TensorOpProduct, 
     TensorOpProductKernel,
-    create, destroy, sigma, share, shift, dim, tensor, 
-    iidx, oidx, lidx,
-    A_mul_B_generated!, make_mul_kernel, make_plus_mul_kernel
+    create, destroy, sigma, 
+    share, dim, tensor, 
+    iidx, oidx
 
 import Base: (*), transpose, conj, ctranspose, kron, A_mul_B!, sparse, full
 
@@ -21,72 +21,8 @@ immutable DiagonalOp{T<:Number,J,K,M} <: TensorOp
 end
 
 
-function share(a::SharedArray)
-    a
-end
-
-function share(a)
-    as = SharedArray(eltype(a),size(a)...)
-    as[:] = a[:]
-    as
-end
-
-
-function share{T,J,K,M}(op::DiagonalOp{T,J,K,M})
-    DiagonalOp{T,J,K,M}(share(op.vals))
-end
-
-function shift{T,J,K,M}(::DiagonalOp{T,J,K,M})
-    M
-end
-
-function dim{T,J,K,M}(d::DiagonalOp{T,J,K,M})
-    J+length(d.vals)+K
-end
-
-
-function iidx{T,J,K,M}(d::DiagonalOp{T,J,K,M})
-    # range of input indices that are processed in own dimension.
-    (1:length(d.vals))+(J-M)
-end
-
-function oidx{T,J,K,M}(d::DiagonalOp{T,J,K,M})
-    # range of output indices that are processed in own dimension.
-    (1:length(d.vals))+J
-end
-
-function lidx{T,J,K,M}(d::DiagonalOp{T,J,K,M})
-    # total length of processed input/output in own dimension.
-    length(d.vals)
-end
-
 type IdentityOp{N} <: TensorOp
     # Identity operator with given dimension N
-end
-
-function share(op::IdentityOp)
-    op
-end
-
-
-function shift{N}(::IdentityOp{N})
-    0
-end
-
-function dim{N}(::IdentityOp{N})
-    N
-end
-
-function iidx{N}(::IdentityOp{N})
-    1:N
-end
-
-function oidx{N}(::IdentityOp{N})
-    1:N
-end
-
-function lidx{N}(::IdentityOp{N})
-    N
 end
 
 type TensorOpProduct
@@ -101,26 +37,35 @@ immutable TensorOpProductKernel{K}
     ops::NTuple{K,Union{DenseVector,IdentityOp}}
     iidx::NTuple{K,Range{Int64}}
     oidx::NTuple{K,Range{Int64}}
-    lidx::NTuple{K, Int64}
 end
 
 function TensorOpProductKernel(tp::TensorOpProduct)
     iidxs = iidx(tp)
     oidxs = oidx(tp)
-    lidxs = lidx(tp)
     ops_red = [(isa(op, DiagonalOp)? op.vals : op) for op in tp.ops]
     M = length(tp.ops)
-    k = TensorOpProductKernel{M}(tuple(ops_red...), tuple(iidxs...), tuple(oidxs...), tuple(lidxs...))
+    k = TensorOpProductKernel{M}(tuple(ops_red...), tuple(iidxs...), tuple(oidxs...))
     tp.kernel = k
     k
 end
 
-function apply_serial!(coeffs, toks::Vector{TensorOpProductKernel}, B::DenseArray, C::DenseArray, c=0)
-    A_mul_B_generated!(coeffs[1], toks[1], B, c, C)
-    for kk=2:length(toks)
-        A_mul_B_generated!(coeffs[kk], toks[kk], B, 1, C)
-    end
-    C
+function share(a::SharedArray)
+    a
+end
+
+function share(a::Array)
+    as = SharedArray(eltype(a),size(a)...)
+    as[:] = a[:]
+    as
+end
+
+function share(op::IdentityOp)
+    op
+end
+
+
+function share{T,J,K,M}(op::DiagonalOp{T,J,K,M})
+    DiagonalOp{T,J,K,M}(share(op.vals))
 end
 
 
@@ -129,92 +74,56 @@ function share(tp::TensorOpProduct)
 end
 
 function share(tpk::TensorOpProductKernel)
-    TensorOpProductKernel{length(tpk.ops)}(tuple(map(share, tpk.ops)), tpk.iidx, tpk.oidx, tpk.lidx) 
+    TensorOpProductKernel{length(tpk.ops)}(tuple(map(share, tpk.ops)), tpk.iidx, tpk.oidx) 
 end
 
 
-function shift(tp::TensorOpProduct)
-    Int64[shift(op) for op=tp.ops]
+
+
+function dim{T,J,K,M}(d::DiagonalOp{T,J,K,M})
+    J+length(d.vals)+K
+end
+
+function dim{N}(::IdentityOp{N})
+    N
 end
 
 
-for fn in [:iidx, :oidx, :lidx]
-    @eval function $fn(tp::TensorOpProduct)
-        map($fn, tp.ops)
-    end
+
+# range of input indices that are processed in own dimension.
+iidx{T,J,K,M}(d::DiagonalOp{T,J,K,M}) = (1:length(d.vals))+(J-M)
+iidx{N}(::IdentityOp{N}) = 1:N
+
+# range of output indices that are processed in own dimension.
+oidx{T,J,K,M}(d::DiagonalOp{T,J,K,M}) =  (1:length(d.vals))+J
+oidx{N}(::IdentityOp{N})=1:N
+
+
+for fn in [:iidx, :oidx]
+    @eval $fn(tp::TensorOpProduct) = map($fn, tp.ops)
 end
 
-function ltot(tp::TensorOpProduct)
-    prod(lidx(tp))
-end
 
-
-import Base: sparse, full
-
-
-function sparse{N}(::IdentityOp{N})
-    speye(N)
-end
+sparse{N}(::IdentityOp{N})= speye(N)
 
 function sparse{T,J,K,M}(d::DiagonalOp{T,J,K,M})
     N = dim(d)
     spdiagm([zeros(T,J);d.vals; zeros(T,K)], 0,N,N) * spdiagm(ones(N-abs(M)),-M, N, N)
 end
+sparse(tp::TensorOpProduct) = reduce(kron, (SparseMatrixCSC)[sparse(op) for op in reverse(tp.ops)])
     
-function full(d::TensorOp)
-    full(sparse(d))
-end
+full(d::Union{TensorOp, TensorOpProduct}) = full(sparse(d))
 
 
-function sparse(tp::TensorOpProduct)
-    reduce(kron, (SparseMatrixCSC)[sparse(op) for op in reverse(tp.ops)])
-end
+create(N::Int) = DiagonalOp{Float64,1,0,1}(sqrt(1:(N-1)))
+destroy(N::Int) = DiagonalOp{Float64,0,1,-1}(sqrt(1:(N-1)))
+sigma(N::Int,j::Int,k::Int) = DiagonalOp{Float64,j-1,N-j,j-k}([1.])
 
-function full(tp::TensorOpProduct)
-    full(sparse(tp))
-end
-
-function create(N::Int)
-    DiagonalOp{Float64,1,0,1}(sqrt(1:(N-1)))
-end
-
-function destroy(N::Int)
-    DiagonalOp{Float64,0,1,-1}(sqrt(1:(N-1)))
-end
-    
-function sigma(N::Int,j::Int,k::Int)
-    DiagonalOp{Float64,j-1,N-j,j-k}([1.])
-end
-
-
-
-
-
-function transpose(ii::IdentityOp)
-    ii
-end
-
-function transpose{T,J,K,M}(d::DiagonalOp{T,J,K,M})
-    DiagonalOp{T,J-M,K+M,-M}(d.vals)
-end
-
-
-function conj(ii::IdentityOp)
-    ii
-end
-
-function conj{T,J,K,M}(d::DiagonalOp{T,J,K,M})
-    if T <: Real 
-        return d 
-    end
-    DiagonalOp{T,J,K,M}(conj(d.vals))
-end
-
-
-function ctranspose(op::TensorOp)
-    transpose(conj(op))
-end
-
+transpose(ii::IdentityOp) = ii
+transpose{T,J,K,M}(d::DiagonalOp{T,J,K,M}) =  DiagonalOp{T,J-M,K+M,-M}(d.vals)
+conj(ii::IdentityOp) = ii
+conj{T,J,K,M}(d::DiagonalOp{T,J,K,M}) = DiagonalOp{T,J,K,M}(conj(d.vals))
+ctranspose(op::TensorOp)= transpose(conj(op))
 
 for fn in [:transpose, :conj, :ctranspose]
     @eval function $fn(tp::TensorOpProduct)
@@ -238,7 +147,6 @@ function (*){M}(a::TensorOp, b::IdentityOp{M})
     N == M || error("Wrong shape for multiplication: ($N x $N) * ($M x $M)")
     a
 end
-
 
 function (*){S,T,J1,K1,M1,J2,K2,M2}(a::DiagonalOp{S,J1,K1,M1}, b::DiagonalOp{T,J2,K2,M2})
     N, M = dim(a), dim(b)
@@ -269,151 +177,32 @@ function tensor(a::TensorOpProduct, b::TensorOp)
     TensorOpProduct([a.ops; b])
 end
 
-TensorType = Union{TensorOp, TensorOpProduct}
-
-function kron(a::TensorType, b::TensorType...)
-    tensor(reverse(b)...,a)
-end
-
-
-function A_mul_B!{T,N}(C::DenseArray{T,N}, A::TensorOpProduct, B::DenseArray{T,N})
-    N == length(A.ops) || error("Mismatch between tensor factors")
-    size(B) === size(C) || error("Dimension mismatch between input and output")
-    iidxs = iidx(A)
-    oidxs = oidx(A)
-    reshaped_ops = []
-    for (k,o)=enumerate(A.ops)
-        if !isa(o, IdentityOp)
-            push!(reshaped_ops, reshape(o.vals, [ones(Int64,k-1);lidx(o); ones(Int64,N-k)]...))
-        end
-    end
-    broadcast!(*, sub(C, oidxs...), sub(B, iidxs...), reshaped_ops...)
-    C
-end
-
-function make_mul_kernel(A::TensorOpProduct)
-    M = length(A.ops)
-    iidxs = iidx(A)
-    oidxs = oidx(A)
-    reshaped_ops = []
-    for (k,o)=enumerate(A.ops)
-        if !isa(o, IdentityOp)
-            push!(reshaped_ops, reshape(o.vals, [ones(Int64,k-1);lidx(o); ones(Int64,M-k)]...))
-        end
-    end
-
-    function kernel!{T,N}(C::DenseArray{T,N}, B::DenseArray{T,N})
-        N == length(A.ops) || error("Mismatch between tensor factors")
-        size(B) === size(C) || error("Dimension mismatch between input and output")
-        broadcast!(*, sub(C, oidxs...), sub(B, iidxs...), reshaped_ops...)
-        C
-    end
-end    
-
-
-    
-
-function plus_mul(a, b...)
-    a + (*)(b...)
-end
-
-const b_plus_mul! = broadcast!_function(plus_mul)
-
-
-function make_plus_mul_kernel(T::DataType, A::TensorOpProduct)
-    mul_kernel! = make_mul_kernel(A)
-
-    M = length(A.ops)
-    iidxs = iidx(A)
-    oidxs = oidx(A)
-    reshaped_ops = []
-    for (k,o)=enumerate(A.ops)
-        if !isa(o, IdentityOp)
-            push!(reshaped_ops, reshape(o.vals, [ones(Int64,k-1);lidx(o); ones(Int64,M-k)]...))
-        end
-    end
-    push!(reshaped_ops, reshape(collect(one(T)),ones(Int64,M)...))
-
-    function kernel!{S,N}(a::T, B::DenseArray{S,N}, c::S, C::DenseArray{S,N})
-        N == length(A.ops) || error("Mismatch between tensor factors")
-        size(B) === size(C) || error("Dimension mismatch between input and output")
-
-        if c != one(S)
-            if c == zero(S)
-                fill!(C, c)
-            else
-                scale!(C, c)
-            end
-        end
-
-        if a == zero(T)
-            return C
-        elseif a == one(T)
-            return mul_kernel!(C, B)
-        end
-        reshaped_ops[end][1] = a
-
-        b_plus_mul!(sub(C, oidxs...), sub(C, oidxs...), sub(B, iidxs...), reshaped_ops...)
-        C
-    end
-end    
-
-
-
-function A_mul_B!{T,N}(a::T, A::TensorOpProduct, B::DenseArray{T,N}, c::T, C::DenseArray{T,N})
-
-    N == length(A.ops) || error("Mismatch between tensor factors")
-    size(B) === size(C) || error("Dimension mismatch between input and output")
-
-    if c != one(T)
-        if c == zero(T)
-            fill!(C, c)
-        else
-            scale!(C, c)
-        end
-    end
-
-    if a == zero(T)
-        return C
-    end
-
-    iidxs = iidx(A)
-    oidxs = oidx(A)
-
-    reshaped_ops = []
-    for (k,o)=enumerate(A.ops)
-        if !isa(o, IdentityOp)
-            push!(reshaped_ops, reshape(o.vals, [ones(Int64,k-1);lidx(o); ones(Int64,N-k)]...))
-        end
-    end
-    push!(reshaped_ops, reshape(collect(a), ones(Int64, N)...))
-    b_plus_mul!(sub(C, oidxs...), sub(C, oidxs...), sub(B, iidxs...), reshaped_ops...)
-    C
+function tensor{S,T,M,N}(A::Array{T,M}, B::Array{S,N})
+    broadcast(*, reshape(A, size(A)...,ones(Int64,N)...),reshape(B,ones(Int64,M)...,size(B)...))
 end
 
 
 
-function A_mul_B_generated!(a, A::TensorOpProduct, B::DenseArray, c, C::DenseArray)
+# In-place mat-vec product without coefficients
+A_mul_B!(C::DenseArray, A::Union{TensorOpProduct,TensorOpProductKernel}, B::DenseArray) = A_mul_B!(1, A, B, 0, C)
+
+function A_mul_B!(a, A::TensorOpProduct, B::DenseArray, c, C::DenseArray)
     k = A.kernel === nothing ? TensorOpProductKernel(A) : A.kernel
-    A_mul_B_generated!(a, k, B, c, C)
+    A_mul_B!(a, k, B, c, C)
 end
 
-function A_mul_B_generated!{K}(a, A::TensorOpProductKernel{K}, B::DenseArray, c, C::DenseArray)
+function A_mul_B!{K}(a, A::TensorOpProductKernel{K}, B::DenseArray, c, C::DenseArray)
     ndims(B) == K || error("Mismatch between tensor factors")
     size(B) === size(C) || error("Dimension mismatch between input and output")
     top_kernel!(c, sub(C, A.oidx...), sub(B, A.iidx...), a, A.ops...)
     C
 end
 
-
-
+# code for generating mat-vec loops
 @generated function top_kernel!(c, sC, sB, a, Avs::Union{DenseVector,IdentityOp}...)
    code = top_kernel!_code(c, sC, sB, a, Avs)
-   println(code)
    code
 end
-
-
 function top_kernel!_code(c, sC, sB, a, Avs)
     K = length(Avs)
     @assert K >= 1
@@ -448,10 +237,10 @@ end
 
 
 
-
-function tensor{S,T,M,N}(A::Array{T,M}, B::Array{S,N})
-    broadcast(*, reshape(A, size(A)...,ones(Int64,N)...),reshape(B,ones(Int64,M)...,size(B)...))
+function apply_serial!(coeffs, toks::Vector{TensorOpProductKernel}, B::DenseArray, C::DenseArray, c=0)
+    A_mul_B_generated!(coeffs[1], toks[1], B, c, C)
+    for kk=2:length(toks)
+        A_mul_B_generated!(coeffs[kk], toks[kk], B, 1, C)
+    end
+    C
 end
-
-
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -34,14 +34,7 @@ function test_mul()
     @test norm((a*a' |> full) -(a|> full)*(a|> full)')<tol
 end
 
-function test_kron()
-    N1 = 5
-    N2 = 10
-    tol = 1e-10
-    a1 = destroy(N1)
-    a2 = destroy(N2)
-    @test norm((kron(a1,a2) |> full) - kron(a1|>full, a2|> full)) < tol
-end
+
 
 function test_A_mul_B(N1=10,N2=10)
     tol = 1e-5
@@ -51,10 +44,12 @@ function test_A_mul_B(N1=10,N2=10)
     p1 = randn(N1)
     p2 = randn(N2)
     x = tensor(p1, p2)
-    println("Tensor prod:")
+
+
+
+    println("Tensor prod with allocation:")
     y1 = zeros(N1,N2)
     A_mul_B!(y1, a1ad2, x)
-
     @time begin 
         y1 = zeros(N1,N2)
         A_mul_B!(y1, a1ad2, x)
@@ -67,199 +62,17 @@ function test_A_mul_B(N1=10,N2=10)
     end
     @test norm(y1[:] - y5[:]) < tol
 
-
-    println("Tensor prod kernel:")
-    y6 = zeros(N1,N2)
-    k! = make_mul_kernel(a1ad2)
-    k!(y6, x)
-    @time begin 
-        k!(y6, x)
-    end
-    @test norm(y1[:] - y6[:]) < tol
-
-
-
-    println("Tensor prod with addition:")
-    y4 = zeros(N1,N2)
-    A_mul_B!(2., a1ad2, x, 3., y4)
-
-    @time begin 
-        y4 = zeros(N1,N2)
-        A_mul_B!(2., a1ad2, x, 3., y4)
-    end
-    @test norm(2*y1[:] - y4[:]) < tol
-
-
-    println(methods(TensorOpProductKernel))
-    println("TensorOpProductKernel:")
+    println("Direct usage of TensorOpProductKernel:")
     y10 = zeros(N1,N2)
-    A_mul_B_generated!(2., a1ad2, x, 3., y10)
-    A_mul_B_generated!(2., a1ad2.kernel, x, 3., y10)
+    A_mul_B!(2., a1ad2.kernel, x, 3., y10)
     y10 = zeros(N1,N2)
     gc()
     @time begin     
-        A_mul_B_generated!(2., a1ad2.kernel, x, 3., y10)
+        A_mul_B!(2., a1ad2.kernel, x, 3., y10)
     end
     @test norm(2*y1[:] - y10[:]) < tol
 
     
-
-    println("Tensor prod with addition (inplace):")
-    gc()
-    @time begin 
-        A_mul_B!(2., a1ad2, x, 3., y4)
-    end
-    @test norm(8*y1[:] - y4[:]) < tol
-
-    println("Tensor prod kernel with addition:")
-    y7 = zeros(N1,N2)
-    k! = make_plus_mul_kernel(Float64,a1ad2)
-    k!(2., x, 3., y7)
-    gc()
-    @time begin 
-        k!(2., x, 3., y7)
-    end
-    @test norm(8*y1[:] - y7[:]) < tol
-
-
-
-
-    # function kernel!{T}(a::T, Av1::Vector{T}, Av2::Vector{T}, sB::SubArray{T,2}, sC::SubArray{T,2})
-    #     for k2 = 1:size(sC,2)
-    #         @simd for k1 = 1:size(sC,1)
-    #             @inbounds sC[k1,k2] = sC[k1,k2] + a * Av1[k1] * Av2[k2] * sB[k1,k2]
-    #         end
-    #     end
-    # end
-
-    function kernel1!(a, Av1, Av2, sB, sC)
-        for k2 = 1:size(sC,2)
-            @simd for k1 = 1:size(sC,1)
-                @inbounds sC[k1,k2] = sC[k1,k2] + a * Av1[k1] * Av2[k2] * sB[k1,k2]
-            end
-        end
-    end
-
-
-    function custom_A_mul_B2_1!{T}(a::T, A, B::Array{T,2}, c::T, C::Array{T,2})
-        size(B) == size(C) || error("Incompatible sizes")
-        if c != one(T)
-            if c == zero(T)
-                fill!(C,c)
-            else 
-                scale!(C,c)
-            end 
-        end 
-        iidx1, iidx2 = iidx(A)
-        oidx1, oidx2 = oidx(A)
-
-        Av1 = (A.ops[1].vals)::Array{T,1}
-        Av2 = (A.ops[2].vals)::Array{T,1}
-
-        sB = sub(B, iidx1, iidx2)
-        sC = sub(C, oidx1, oidx2)
-
-        kernel1!(a, Av1, Av2, sB, sC)
-        C
-    end
-
-
-    println("Custom function 1:")
-    y8 = zeros(N1,N2)
-    custom_A_mul_B2_1!(2., a1ad2, x, 3., y8)
-    gc()
-    @time begin 
-        custom_A_mul_B2_1!(2., a1ad2, x, 3., y8)
-    end
-    @test norm(8*y1[:] - y8[:]) < tol
-
-    y8 = 0
-    gc()
-
-    function kernel2!(a, Av1, Av2, sB, c, sC)
-        for k2 = 1:size(sC,2)
-            @simd for k1 = 1:size(sC,1)
-                @inbounds sC[k1,k2] = c * sC[k1,k2] + a * Av1[k1] * Av2[k2] * sB[k1,k2]
-            end
-        end
-    end
-
-
-    function custom_A_mul_B2_2!{T}(a::T, A, B::Array{T,2}, c::T, C::Array{T,2})
-        size(B) == size(C) || error("Incompatible sizes")
-        iidx1, iidx2 = iidx(A)
-        oidx1, oidx2 = oidx(A)
-
-        Av1 = (A.ops[1].vals)::Array{T,1}
-        Av2 = (A.ops[2].vals)::Array{T,1}
-
-        sB = sub(B, iidx1, iidx2)
-        sC = sub(C, oidx1, oidx2)
-
-        kernel2!(a, Av1, Av2, sB, c, sC)
-        C
-    end
-
-
-    println("Custom function 2:")
-    y8 = zeros(N1,N2)
-    custom_A_mul_B2_2!(2., a1ad2, x, 3., y8)
-    gc()
-    @time begin 
-        custom_A_mul_B2_2!(2., a1ad2, x, 3., y8)
-    end
-    @test norm(8*y1[:] - y8[:]) < tol
-
-    y8 = 0
-    gc()
-
-    # function kernel_p!(a, Av1, Av2, sB, sC)
-    #     @sync @parallel for k2 = 1:size(sC,2)
-    #         @simd for k1 = 1:size(sC,1)
-    #             @inbounds sC[k1,k2] = sC[k1,k2] + a * Av1[k1] * Av2[k2] * sB[k1,k2]
-    #         end
-    #     end
-    # end
-
-
-    # function custom_A_mul_B2_p!{T}(a::T, A, B::SharedArray{T,2}, c::T, C::SharedArray{T,2})
-    #     size(B) == size(C) || error("Incompatible sizes")
-    #     if c != one(T)
-    #         if c == zero(T)
-    #             fill!(C,c)
-    #         else 
-    #             scale!(C,c)
-    #         end 
-    #     end 
-    #     iidx1, iidx2 = iidx(A)
-    #     oidx1, oidx2 = oidx(A)
-
-    #     Av1 = (A.ops[1].vals)::SharedArray{T,1}
-    #     Av2 = (A.ops[2].vals)::SharedArray{T,1}
-
-    #     sB = sub(B, iidx1, iidx2)
-    #     sC = sub(C, oidx1, oidx2)
-
-    #     kernel_p!(a, Av1, Av2, sB, sC)
-    #     C
-    # end
-
-
-    # println("Custom function parallel:")
-    # y9 = SharedArray(Float64, N1, N2)
-    # @test norm(y9) == 0
-    # xp = share(x)
-    # @test norm(xp[:] - x[:]) == 0
-    # a1ad2p = share(a1ad2)
-    # custom_A_mul_B2_p!(2., a1ad2p, xp, 3., y9)
-    # @time begin 
-    #     custom_A_mul_B2_p!(2., a1ad2p, xp, 3., y9)
-    # end
-    # @test norm(8*y1[:] - y9[:]) < tol
-
-    # xp = 0
-    # y9 = 0
-    # gc()
 
     println("Tensorproduct of individual products:")
     y2 = tensor(((a1|>sparse)*p1),((ad2|>sparse)*p2))
@@ -300,7 +113,6 @@ test_to_sparse()
 test_to_full()
 test_transpose()
 test_mul()
-test_kron()
-test_A_mul_B(10000,5000)
+test_A_mul_B(10000,1000)
 
 
