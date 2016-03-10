3f019bd995e8cf2103a3db0610e4fc7d78fe1a9c
Nikolas Tezak <nikolas.tezak@gmail.com>
2016-03-09 19:00:50 -0800

    added type for tensor product sums

--- src/sparse_tensors.jl
+++ src/sparse_tensors.jl
@@ -7,30 +7,74 @@ export TensorOp, DiagonalOp,
     share, dim, tensor, 
     iidx, oidx
 
-import Base: (*), transpose, conj, ctranspose, kron, A_mul_B!, sparse, full
+import Base: (*), transpose, conj, ctranspose, kron, A_mul_B!, sparse, full, 
+              convert, promote_rule
 
 abstract TensorOp
 
-immutable DiagonalOp{T<:Number,J,K,M} <: TensorOp
+immutable DiagonalOp{T<:Number} <: TensorOp
     # T: data type
     # J: number of zeros before first non-zero
     # K: number of zeros after last non-zero
     # M: Shift offset
     # vals: diagonal elements between the non-zeros
-    vals::Union{Vector{T},SharedVector{T}}
+    vals::Vector{T}
+    jkm::NTuple{3,Int64}
+    # expr::Expr
+    # DiagonalOp{T}(vals::Vector{T}, jkm, expr = :()) = new{T}(vals, jkm, expr)
 end
+
+# function DiagonalOp(T, op::DiagonalOp)
+#   TT = typeof(op)
+#   J,K,M = TT.parameters[2:end]
+#   DiagonalOp{T,J,K,M}(convert(Vector{T}, op.vals))
+# end
+#
+
+promote_rule{T,S}(::Type{DiagonalOp{T}}, ::Type{DiagonalOp{S}}) = DiagonalOp{promote_type(T,S)}
+convert{T}(::Type{DiagonalOp{T}}, op::DiagonalOp) = DiagonalOp(convert(Vector{T}, op.vals), op.jkm)
 
 
 type IdentityOp{N} <: TensorOp
     # Identity operator with given dimension N
 end
 
-type TensorOpProduct
+type TensorOpProduct{T,K}
     # Product of single degree tensor ops
-    ops::Vector{TensorOp}
+    ops::NTuple{K,Union{DiagonalOp{T},IdentityOp}}
     kernel
-    TensorOpProduct(ops) = new(ops, nothing)
 end
+TensorOpProduct(ops::TensorOp...) = TensorOpProduct(promote_diags(ops...), nothing)
+
+
+function promote_diags(ops::TensorOp...)
+  opsv = collect(ops)
+  nontrivial=[isa(op, DiagonalOp) for op=opsv]
+  promoted_opsv = Array(Any,length(ops))
+  promoted_opsv[nontrivial] = collect(promote(opsv[nontrivial]...))
+  promoted_opsv[~nontrivial] = opsv[~nontrivial]
+  tuple(promoted_opsv...)
+end
+
+function convert_diags{T}(S::Type{DiagonalOp{T}}, ops::TensorOp...)
+  nops = Any[]
+  for op in ops
+    if isa(op, DiagonalOp)
+      push!(nops, convert(S, op))
+    else
+      push!(nops, op)
+    end
+  end
+  tuple(nops...)
+end
+
+
+
+promote_rule{T,S,K}(::Type{TensorOpProduct{T,K}}, ::Type{TensorOpProduct{S,K}}) = TensorOpProduct{promote_type(T,S),K}
+promote_rule{T,S}(::Type{TensorOpProduct{T}}, ::Type{TensorOpProduct{S}}) = TensorOpProduct{promote_type(T,S)}
+convert{T,S,K}(::Type{TensorOpProduct{T,K}}, top::TensorOpProduct{S,K}) = TensorOpProduct(convert_diags(DiagonalOp{T}, top.ops...)...)
+convert{T}(::Type{TensorOpProduct{T}}, top::TensorOpProduct) = TensorOpProduct(convert_diags(DiagonalOp{T}, top.ops...)...)
+
 
 immutable TensorOpProductKernel{K}
     # Product of single degree tensor ops
@@ -49,39 +93,59 @@ function TensorOpProductKernel(tp::TensorOpProduct)
     k
 end
 
-function share(a::SharedArray)
-    a
+type TensorOpProductSum{T,K,N}
+    # Sum of product of single degree tensor ops
+    tops::NTuple{N,NTuple{K,Union{IdentityOp,DiagonalOp{T}}}}
+    kernel
 end
 
-function share(a::Array)
-    as = SharedArray(eltype(a),size(a)...)
-    as[:] = a[:]
-    as
+function TensorOpProductSum{K}(tops::TensorOpProduct{K}...)
+  tops = promote(tops...)
+  topops = [top.ops for top in tops]
+  println(topops)
+  TensorOpProductSum(tuple(topops...), nothing)
 end
 
-function share(op::IdentityOp)
-    op
+@generated function apply_topsum!(ts, tops, B::DenseArray, c, C::DenseArray)
+  println(ts, tops, B, c, C)
+  nothing
 end
 
+      
 
-function share{T,J,K,M}(op::DiagonalOp{T,J,K,M})
-    DiagonalOp{T,J,K,M}(share(op.vals))
-end
+# 
+# function share(a::SharedArray)
+#     a
+# end
+# 
+# function share(a::Array)
+#     as = SharedArray(eltype(a),size(a)...)
+#     as[:] = a[:]
+#     as
+# end
+# 
+# function share(op::IdentityOp)
+#     op
+# end
+# 
+# 
+# function share{T,J,K,M}(op::DiagonalOp{T,J,K,M})
+#     DiagonalOp{T,J,K,M}(share(op.vals))
+# end
+# 
+# 
+# function share(tp::TensorOpProduct)
+#     TensorOpProduct(map(share, tp.ops))
+# end
+# 
+# function share(tpk::TensorOpProductKernel)
+#     TensorOpProductKernel{length(tpk.ops)}(tuple(map(share, tpk.ops)), tpk.iidx, tpk.oidx) 
+# end
+# 
 
-
-function share(tp::TensorOpProduct)
-    TensorOpProduct(map(share, tp.ops))
-end
-
-function share(tpk::TensorOpProductKernel)
-    TensorOpProductKernel{length(tpk.ops)}(tuple(map(share, tpk.ops)), tpk.iidx, tpk.oidx) 
-end
-
-
-
-
-function dim{T,J,K,M}(d::DiagonalOp{T,J,K,M})
-    J+length(d.vals)+K
+function dim(d::DiagonalOp)
+  J,K,M = d.jkm
+  J+length(d.vals)+K
 end
 
 function dim{N}(::IdentityOp{N})
@@ -91,11 +155,11 @@ end
 
 
 # range of input indices that are processed in own dimension.
-iidx{T,J,K,M}(d::DiagonalOp{T,J,K,M}) = (1:length(d.vals))+(J-M)
+iidx(d::DiagonalOp) = (1:length(d.vals))+(d.jkm[1]-d.jkm[3])
 iidx{N}(::IdentityOp{N}) = 1:N
 
 # range of output indices that are processed in own dimension.
-oidx{T,J,K,M}(d::DiagonalOp{T,J,K,M}) =  (1:length(d.vals))+J
+oidx(d::DiagonalOp) =  (1:length(d.vals))+d.jkm[1]
 oidx{N}(::IdentityOp{N})=1:N
 
 
@@ -106,8 +170,9 @@ end
 
 # conversion to sparse matrices
 sparse{N}(::IdentityOp{N})= speye(N)
-function sparse{T,J,K,M}(d::DiagonalOp{T,J,K,M})
+function sparse{T}(d::DiagonalOp{T})
     N = dim(d)
+    J,K,M = d.jkm
     spdiagm([zeros(T,J);d.vals; zeros(T,K)], 0,N,N) * spdiagm(ones(N-abs(M)),-M, N, N)
 end
 sparse(tp::TensorOpProduct) = reduce(kron, (SparseMatrixCSC)[sparse(op) for op in reverse(tp.ops)])
@@ -115,19 +180,19 @@ sparse(tp::TensorOpProduct) = reduce(kron, (SparseMatrixCSC)[sparse(op) for op i
 # conversion of full matrices
 full(d::Union{TensorOp, TensorOpProduct}) = d |> sparse |> full
 
-create(N::Int) = DiagonalOp{Float64,1,0,1}(sqrt(1:(N-1)))
-destroy(N::Int) = DiagonalOp{Float64,0,1,-1}(sqrt(1:(N-1)))
-sigma(N::Int,j::Int,k::Int) = DiagonalOp{Float64,j-1,N-j,j-k}([1.])
+create(N::Int) = DiagonalOp{Float64}(sqrt(1:(N-1)),(1,0,1))
+destroy(N::Int) = DiagonalOp{Float64}(sqrt(1:(N-1)),(0,1,-1))
+sigma(N::Int,j::Int,k::Int) = DiagonalOp{Float64}([1.],(j-1,N-j,j-k))
 
 transpose(ii::IdentityOp) = ii
-transpose{T,J,K,M}(d::DiagonalOp{T,J,K,M}) =  DiagonalOp{T,J-M,K+M,-M}(d.vals)
+transpose(d::DiagonalOp) =  DiagonalOp(d.vals,(d.jkm[1]-d.jkm[3],d.jkm[2]+d.jkm[3],-d.jkm[3]))
 conj(ii::IdentityOp) = ii
-conj{T,J,K,M}(d::DiagonalOp{T,J,K,M}) = DiagonalOp{T,J,K,M}(conj(d.vals))
+conj(d::DiagonalOp) = DiagonalOp(conj(d.vals), d.jkm)
 ctranspose(op::TensorOp)= transpose(conj(op))
 
 for fn in [:transpose, :conj, :ctranspose]
     @eval function $fn(tp::TensorOpProduct)
-        TensorOpProduct(TensorOp[$fn(op) for op in tp.ops])
+        TensorOpProduct([$fn(op) for op in tp.ops]...)
     end
 end
 
@@ -150,8 +215,10 @@ function (*){M}(a::TensorOp, b::IdentityOp{M})
     a
 end
 
-function (*){S,T,J1,K1,M1,J2,K2,M2}(a::DiagonalOp{S,J1,K1,M1}, b::DiagonalOp{T,J2,K2,M2})
+function (*){S,T}(a::DiagonalOp{S}, b::DiagonalOp{T})
     N, M = dim(a), dim(b)
+    J1,K1,M1 = a.jkm
+    J2,K2,M2 = b.jkm
     N == M || error("Wrong shape for multiplication: ($N x $N) * ($M x $M)")
     da = [zeros(S, J1); a.vals; zeros(S,K1)]
     db = [zeros(T, J2); b.vals; zeros(T,K2)]
@@ -164,21 +231,21 @@ function (*){S,T,J1,K1,M1,J2,K2,M2}(a::DiagonalOp{S,J1,K1,M1}, b::DiagonalOp{T,J
     q, r = find(abs(dab) .> 0)[[1,end]]
     J = q - 1
     K = N - r
-    DiagonalOp{eltype(dab),J,K,M1+M2}(dab[q:r])
+    DiagonalOp{eltype(dab)}(dab[q:r],(J,K,M1+M2))
 end
 
 
 # construction of tensor products
-function tensor(a::TensorOp, b::TensorOp...)
-    TensorOpProduct([a; b...])
+function tensor(a::TensorOp...)
+    TensorOpProduct(a...)
 end
 
 function tensor(a::TensorOp, b::TensorOpProduct)
-    TensorOpProduct([a; b.ops])
+    TensorOpProduct(a, b.ops...)
 end
 
 function tensor(a::TensorOpProduct, b::TensorOp)
-    TensorOpProduct([a.ops; b])
+    TensorOpProduct(a.ops..., b)
 end
 
 # construction of tensor products for states
